6466dc361865bfa2b16ffcf074f7d91f1aa03711
Ade Widyatama DB <adewidyatamadb@gmail.com>
2021-08-07 17:38:17 +0800

    Creating insert reservation and room restriction function and using them in the handlers, updating the reservation summary page

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,5 +4,8 @@ import "github.com/adewidyatamadb/GoBookings/internal/models"
 
 type DatabaseRepo interface {
 	AllUsers() bool
-	InsertReservation(res models.Reservation) error
+
+	InsertReservation(res models.Reservation) (int, error)
+
+	InsertRoomRestriction(res models.RoomRestriction) error
 }
