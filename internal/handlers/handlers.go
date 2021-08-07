97c7c4e39dd9a75dcbc248fd813da08e3ce0a93d
Ade Widyatama DB <adewidyatamadb@gmail.com>
2021-08-07 17:06:36 +0800

    Testing the insert to reservation table function and updating the make reservation form

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -75,7 +75,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	// 01-01-2021 -- 01/02 03:04:05PM '06 -0700
 
-	layout := "02-01-2021"
+	layout := "02-01-2006"
 	startDate, err := time.Parse(layout, sd)
 	if err != nil {
 		helpers.ServerError(w, err)
