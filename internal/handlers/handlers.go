45e84178eaec2237d3a023b81de1ae0d6376fcdc
Ade Widyatama DB <adewidyatamadb@gmail.com>
2021-08-07 16:45:39 +0800

    Creating insert to database function for reservation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/adewidyatamadb/GoBookings/internal/config"
 	"github.com/adewidyatamadb/GoBookings/internal/driver"
@@ -68,11 +70,34 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	sd := r.Form.Get("start_date")
+	ed := r.Form.Get("end_date")
+
+	// 01-01-2021 -- 01/02 03:04:05PM '06 -0700
+
+	layout := "02-01-2021"
+	startDate, err := time.Parse(layout, sd)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+	endDate, err := time.Parse(layout, ed)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
+	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Email:     r.Form.Get("email"),
 		Phone:     r.Form.Get("phone"),
+		StartDate: startDate,
+		EndDate:   endDate,
+		RoomID:    roomID,
 	}
 
 	form := forms.New(r.PostForm)
@@ -90,6 +115,11 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 			Data: data,
 		})
 		return
+	}
+
+	err = m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
 	}
 
 	m.App.Session.Put(r.Context(), "reservation", reservation)
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,8 @@
 package repository
 
+import "github.com/adewidyatamadb/GoBookings/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+	InsertReservation(res models.Reservation) error
 }
